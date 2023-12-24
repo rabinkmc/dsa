39ee690b2d09c84c29538d11dc9b965908875483
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-24 18:04:49 +0545

    100169: maximize square area

---
+++ 100169.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def maximizeSquareArea(
+        self, m: int, n: int, hFences: List[int], vFences: List[int]
+    ) -> int:
+        def diff(bar, size):
+            ans = set()
+            bar.sort()
+            bars = [1, *bar, size]
+            for i in range(len(bars)):
+                for j in range(i + 1, len(bars)):
+                    ans.add(bars[j] - bars[i])
+
+            return ans
+
+        hbar_diffs = diff(hFences, m)
+        vbar_diffs = diff(vFences, n)
+        diffs = hbar_diffs & vbar_diffs
+        if not diffs:
+            return -1
+        ans = max(diffs)
+        modulo = 1000000007
+        return (ans**2) % modulo
+
+
+m = 6
+n = 7
+hBars = [2]
+vBars = [4]
+
+print(Solution().maximizeSquareArea(m, n, hBars, vBars))
