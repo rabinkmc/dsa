49d5c26066f39d6989d826b86cc982918b6c38e0
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-29 17:18:25 +0545

    has cycles

--- 141-linked-list_cycle.py
+++ 141-linked-list_cycle.py
@@ -2,3 +2,21 @@ from typing import Optional
 from typing import List
 from collections import *
 from itertools import *
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        curr = head
+        seen = set()
+        while curr:
+            if curr in seen:
+                return True
+            seen.add(curr)
+            curr = curr.next
+        return False
+
+
