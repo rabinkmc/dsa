6828ac5559bb2469b9ab6b466378aa2762b5e52c
Rabin Dhamala <rabinkmc@gmail.com>
2023-12-30 23:28:51 +0545

    optimized

--- 542-01-matrix.py
+++ 542-01-matrix.py
@@ -12,29 +12,24 @@ class Solution:
             for j in range(w):
                 if mat[i][j] == 0:
                     queue.append((i, j))
+                else:
+                    mat[i][j] = -1 # track unvisited nodes
         count = 0
         moves = [(-1, 0), (1, 0), (0,-1), (0, 1)]
-        seen = set()
-        output = [[0]*w for _ in range(h)]
         while queue:
-            count += 1
-            for i in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in moves:
-                    rr = r + dr
-                    cc = c + dc
-                    if rr < 0  or rr >= h or cc < 0 or cc >= w:
-                        continue
-                    if mat[rr][cc] == 0:
-                        continue
-                    if (rr, cc) in seen:
-                        continue
-                    output[rr][cc] = count
-                    queue.append((rr, cc))
-                    seen.add((rr, cc))
-        return output
+            r, c = queue.popleft()
+            for dr, dc in moves:
+                rr = r + dr
+                cc = c + dc
+                if rr < 0  or rr >= h or cc < 0 or cc >= w:
+                    continue
+                if mat[rr][cc] != -1:
+                    continue
+                mat[rr][cc] = mat[r][c] + 1
+                queue.append((rr, cc))
+        return mat
 
 mat = [[0,0,0],[0,1,0],[1,1,1]]
 ans = Solution().updateMatrix(mat)
-assert ans == [[0,0,0],[0,1,0],[1,2,1]]
+# assert ans == [[0,0,0],[0,1,0],[1,2,1]], ans
 
