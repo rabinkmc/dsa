d6a52b4499776a8156b71669fecf66a402c470a1
Rabin Dhamala <rabinkmc@gmail.com>
2024-01-02 09:55:33 +0545

    add backtracking template

--- templates/backtrack.py
+++ templates/backtrack.py
@@ -8,3 +8,14 @@ def backtrack(curr, OTHER_ARGUMENTS...):
         ans += backtrack(curr, OTHER_ARGUMENTS...)
         # undo the modification of the current state
     return ans
+
+def backtrack(candidate):
+    if find_solution(candidate):
+        output(candidate)
+        return 
+
+    for next_candidate in list_of_candidates:
+        if is_valid(next_candidate):
+            place(next_candidate)
+            backtrack(next_candidate)
+            remove(next_candidate)
