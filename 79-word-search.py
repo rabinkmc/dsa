b5424ce08a170689b3967b6db1f9e90d241b99bb
Rabin Dhamala <rabinkmc@gmail.com>
2024-01-02 21:28:32 +0545

    79: word search, do a dfs from each start node

--- 79-word-search.py
+++ 79-word-search.py
@@ -6,54 +6,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         h = len(board)
         w = len(board[0])
-        starts = deque()
-        visited = set()
+
+        moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def dfs(r, c, word):
+            if not word:
+                return True
+            if r < 0 or r >= h or c < 0 or c >= w or board[r][c] != word[0]:
+                return False
+
+            temp = board[r][c]
+            board[r][c] = "#"
+            for dr, dc in moves:
+                rr, cc = r + dr, c + dc
+                if dfs(rr, cc, word[1:]):
+                    board[r][c] = temp
+                    return True
+            board[r][c] = temp
+            return False
+
         for i in range(h):
             for j in range(w):
                 if board[i][j] == word[0]:
-                    starts.append((i, j))
-        if not starts:
-            return False
-        moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for start in starts:
-            visited.clear()
-            stack = [(start, 1)]
-            visited.add(start)
-            while stack:
-                (r, c), idx = stack.pop()
-                if idx == len(word):
-                    return True
-                for dr, dc in moves:
-                    rr, cc = r + dr, c + dc
-                    if rr < 0 or rr >= h or cc < 0 or cc >= w:
-                        continue
-                    if (rr, cc) in visited:
-                        continue
-                    if board[rr][cc] == word[idx]:
-                        stack.append(((rr, cc), idx + 1))
-                        visited.add((rr, cc))
+                    if dfs(i, j, word):
+                        return True
         return False
 
-board = [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
-#ABCE
-#SFES
-#ADEE
-word = "ABCESEEEFS"
-ans = Solution().exist(board, word)
-assert ans == True
-
-board = [["C","A","A"],["A","A","A"],["B","C","D"]]
-#CAA
-#AAA
-#BCD
-word = "AAB"
-ans = Solution().exist(board, word)
-assert ans == True
-
-board = [["a", "a"]]
-word = "aa"
-ans = Solution().exist(board, word)
-assert ans == True
 
 board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
 word = "SEE"
@@ -65,3 +43,35 @@ word = "aba"
 ans = Solution().exist(board, word)
 assert ans == False
 
+board = [
+    ["A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A"],
+]
+word = "AAAAAAAAAAAABAA"
+ans = Solution().exist(board, word)
+assert ans == False
+
+board = [["A", "B", "C", "E"], ["S", "F", "E", "S"], ["A", "D", "E", "E"]]
+# ABCE
+# SFES
+# ADEE
+word = "ABCESEEEFS"
+ans = Solution().exist(board, word)
+assert ans == True
+
+board = [["C", "A", "A"], ["A", "A", "A"], ["B", "C", "D"]]
+# CAA
+# AAA
+# BCD
+word = "AAB"
+ans = Solution().exist(board, word)
+assert ans == True
+
+board = [["a", "a"]]
+word = "aa"
+ans = Solution().exist(board, word)
+assert ans == True
