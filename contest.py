74b716a4c3ac3dc6694c054186e92348a01c474f
Rabin Dhamala <rabinkmc@gmail.com>
2024-01-05 19:32:07 +0545

    contest setup
    1. `contests.md` will hold information of all the contests
    2. `contest.md` will hold information of the latest contest
    3. contest.py will read from latest contest file and create folder and
       files for those. And it prints the files created so that it can be
       piped and opened in nvim via xargs

---
+++ contest.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+folder,*problems  = open("contest.md").read().strip("\n").split("\n")
+import os
+contest_dir = folder.split("#")[-1].strip()
+path = Path(f"contests/{contest_dir}")
+
+path.mkdir(exist_ok=True)
+
+for problem in problems:
+    name = problem.split(".")[-1].strip().lower().replace(" ", "-") + ".py"
+    print(path / name)
+
+
+
---
+++ contests.md
@@ -0,0 +1,4 @@
+# 358
+1. Max Pair Sum in an Array
+2. Double a Number Represented as a Linked List
+3. Minimum Absolute Difference Between Elements With Constraint 
