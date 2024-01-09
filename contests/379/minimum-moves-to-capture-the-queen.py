c4d6fac43dc9fe99e1ece37685cb97982ca7c8b3
Rabin Dhamala <rabinkmc@gmail.com>
2024-01-09 09:13:48 +0545

    update minimum moves to capture the queen

--- contests/379/minimum-moves-to-capture-the-queen.py
+++ contests/379/minimum-moves-to-capture-the-queen.py
@@ -3,24 +3,22 @@ class Solution:
         self, a: int, b: int, c: int, d: int, e: int, f: int
     ) -> int:
         if a == e or b == f:
-            if not (a == c or b == d):
-                return 1
-            if not ((a > c > e) or (a < c < e) or (b > d > f) or (b < d < f)):
-                return 1
-            if abs(c - e) % 2 == 0 or abs(b - f) % 2 == 0:
-                return 2
-            return 3
+            if (a == c or b == d):
+                if (a > c > e) or (a < c < e) or (b > d > f) or (b < d < f):
+                    return 2
+            return 1
+
         diag1 = [list(range(i, i + 8)) for i in range(1, 8 + 1)]
         diag2 = diag1[::-1]
+
         a, b, c, d, e, f = a - 1, b - 1, c - 1, d - 1, e - 1, f - 1
         rook_on_bishop_line = diag1[c][d] == diag1[a][b] or diag2[c][d] == diag2[a][b]
         if diag1[e][f] == diag1[c][d] or diag2[e][f] == diag2[c][d]:
-            if not rook_on_bishop_line:
-                return 1
-            # queen > rook > bishop
-            # bishop > rook > queen
-            if (e > a > c) or (e < a < c) or (f > b > d) or (f < b < d):
-                return 2
+            if rook_on_bishop_line:
+                # queen > rook > bishop
+                # bishop > rook > queen
+                if (e > a > c) or (e < a < c) or (f > b > d) or (f < b < d):
+                    return 2
             return 1
         return 2
 
