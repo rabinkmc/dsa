854796acc8bdabdb578f4b57c62cc9a157db22c4
Rabin Dhamala <rabinkmc@gmail.com>
2024-01-19 16:59:46 +0545

    updates

--- templates/dijkstra.py
+++ templates/dijkstra.py
@@ -1,17 +1,22 @@
 from math import inf
-from heapq import *
+from heapq import heappush, heappop
 
-distances = [inf] * n
-distances[source] = 0
-heap = [(0, source)]
+def next_candidates(node):
+    # implement your node
+    return node
 
-while heap:
-    curr_dist, node = heapop(heap)
-    if curr_dist > distances[node]:
-        continue
-
-    for next_node, weight in graph[node]:
-        dist = curr_dist + weight
-        if dist < distances[next_node]:
-            distances[next_node] = dist
-            heappush(heap, (dist, next_node))
+def dijkstra(source, end):
+    distances = dict()
+    # initialize all the distances of node to inf
+    distances[source] = 0 # set source distance to 0
+    heap = [(source, 0)] # push the source node
+    while heap:
+        node, curr_dist = heappop(heap) # process the node
+        if curr_dist > distances[node]:
+            continue
+        for next_node, weight in next_candidates(node):
+            dist = curr_dist + weight
+            if dist < distances[next_node]:
+                distances[next_node] = dist
+                heappush(heap, (next_node, dist))
+    return distances[end]
